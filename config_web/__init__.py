c169405d573872afcd61f3090899f6bae01f8762
Chunlei Wu <anewgene@yahoo.com>
2021-12-09 17:05:26 -0800

    initial commit

---
+++ config_web/__init__.py
@@ -0,0 +1,1 @@
+from .config_web_private import *
---
+++ index.py
@@ -0,0 +1,5 @@
+from biothings.web.index_base import main
+from web.handlers import EXTRA_HANDLERS
+
+if __name__ == "__main__":
+    main(EXTRA_HANDLERS)
