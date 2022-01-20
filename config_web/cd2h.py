170d087eafceedc342586ee3d3e61db78ab92953
Nichollette <nichollette.aco@gmail.com>
2022-01-20 17:05:14 -0500

    adding folder restructure which matches pending api strucutre

--- config_web/cd2h.py
+++ config_web/cd2h.py
@@ -4,7 +4,7 @@ from web.handlers import EXTRA_HANDLERS
 from biothings.web.settings.default import APP_LIST
 
 
-ES_HOST= 'localhost:9200'
+ES_HOST= 'http://search.cd2h.org:9200/
 ES_INDICES = {"es_query": "v1"} # document type URL templating
 ANNOTATION_DEFAULT_SCOPES = ["_id", "symbol"] # setting to specify id fields 
 #ES_INDEX=''
@@ -13,10 +13,10 @@ ANNOTATION_DEFAULT_SCOPES = ["_id", "symbol"] # setting to specify id fields
 API_PREFIX='cd2h'
 API_VERSION=''
 # Declare a new route
-APP_LIST = [
-    *APP_LIST, # keep the original ones
-    (r"/{ver}/es_query/(.+)", "web.handlers.EchoHandler"),
-]
+#APP_LIST = [
+ #   *APP_LIST, # keep the original ones
+ #   (r"/{ver}/es_query/(.+)", "web.handlers.EchoHandler"),
+#]
 
 ES_RESULT_TRANSFORM = "web.pipeline.MyFormatter" # format the output
 ES_QUERY_BUILDER = "web.pipeline.MyQueryBuilder" # increase search result relevancy 
