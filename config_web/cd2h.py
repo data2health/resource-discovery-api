89a8ffc31b7af2b604e828a2c39c6cf2fec7ab5d
Chunlei Wu <anewgene@yahoo.com>
2022-01-26 18:49:54 -0800

    some minor config changes

--- config_web/cd2h.py
+++ config_web/cd2h.py
@@ -4,11 +4,16 @@ from config_local import ES_PRIVATE_HOST, ES_HTTP_AUTH
 # Elasticsearch Settings
 # *****************************************************************************
 ES_HOST = ES_PRIVATE_HOST
-ES_ARGS={
+ES_ARGS = {
     "http_auth": ES_HTTP_AUTH
 }
 
-ES_INDICES = {"cd2h-outbreak": "outbreak_*"}
+ES_INDICES = {
+    None: "c*,o*",   # all indices excluding internal ones
+    "outbreak": "outbreak_*",
+    "cd2h": "cd2h-*",
+    "csbc": "csbc-*"
+}
 ES_DOC_TYPE = 'doc'
 ANNOTATION_DEFAULT_SCOPES = ["_id"]
 
@@ -16,6 +21,5 @@ ANNOTATION_DEFAULT_SCOPES = ["_id"]
 # *****************************************************************************
 # Web Application
 # *****************************************************************************
-API_PREFIX='cd2h-outbreak'
-API_VERSION = 'v1'
-
+API_PREFIX = 'api'
+API_VERSION = ''
