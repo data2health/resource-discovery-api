c44b5bccc23532cfba41d9ea511786fd44970f61
Nichollette <nichollette.aco@gmail.com>
2022-06-21 23:12:51 -0400

    added extra fields

--- web/pipeline.py
+++ web/pipeline.py
@@ -13,7 +13,7 @@ class RDPQueryBuilder(ESQueryBuilder):
         if ":" in q or " AND " in q or " OR " in q:
             search = search.query('query_string', query=q)
         
-        # Boost fields 
+        # Boost fields : ['name','title', 'label', 'toolName', 'article_title']
         # Note: 'author' field goes by various names across indices, ['name', 'etc'...],
         # therefore we have to account for the variety
         # -- term search 
@@ -24,7 +24,12 @@ class RDPQueryBuilder(ESQueryBuilder):
                         "queries": [
                             {"term": {"author.name": {"value": q.strip('"'), "boost": 5}}},
                             {"term": {"name": {"value": q.strip('"'), "boost": 5}}},
-                            {"match": {"author.name": {"query": q, "boost": 3, "operator": "AND"}}},
+                            {"term": {"label": {"value": q.strip('"'), "boost": 5}}},
+                            {"term": {"toolName": {"value": q.strip('"'), "boost": 5}}},
+                            {"term": {"article_title": {"value": q.strip('"'), "boost": 5}}},
+                            #{"match": {"author.name": {"query": q, "boost": 3, "operator": "AND"}}},
+                            {"multi_match": {"fields": ['author.name', 'title', 'name', 'label', 'toolName', 'article_title'], "query": q, "boost": 5, "operator": "AND"}},
+                            # ---------------------------------------------
                             {"query_string": {"query": q, "default_operator": "AND"}}  # base score
                         ]
                     }
@@ -38,14 +43,20 @@ class RDPQueryBuilder(ESQueryBuilder):
                 "query": {
                     "dis_max": {
                         "queries": [
-                            {"term": { "author.name": {"value": q, "boost": 5}}},
-                            {"term": {"name": {"value": q, "boost": 5}}},
-                            {"match": {"author.name": {"query": q, "boost": 3}}},
+                            {"term": {"author.name": {"value": q.strip('"'), "boost": 5}}},
+                            {"term": {"name": {"value": q.strip('"'), "boost": 5}}},
+                            {"term": {"label": {"value": q.strip('"'), "boost": 5}}},
+                            {"term": {"toolName": {"value": q.strip('"'), "boost": 5}}},
+                            {"term": {"article_title": {"value": q.strip('"'), "boost": 5}}},
+                            #{"match": {"author.name": {"query": q, "boost": 3}}},
+                            {"multi_match": {"fields": ['author.name', 'title', 'name', 'label', 'toolName', 'article_title'], "query": q, "boost": 5, "operator": "AND"}},
                             # ---------------------------------------------
                             {"query_string": {"query": q, "default_field": "all"}},  # base score
                             # ---------------------------------------------
-                            {"wildcard": {"author.name": {"value": q + "*", "boost": 2}}},
-                            {"wildcard": {"title": {"value": q + "*", "boost": 0.5}}}
+                            {"wildcard": {"title": {"value": q + "*", "boost": 3}}},
+                            {"wildcard": {"label": {"value": q + "*", "boost": 3}}},
+                            {"wildcard": {"article_title": {"value": q + "*", "boost": 3}}},
+                            {"wildcard": {"toolName": {"value": q + "*", "boost": 3}}}
                         ],
                     }
                 }
